d59356b1fb990dac89ed0841de8b94578a5dde67
bvquoc <bvquoc.dev@gmail.com>
2022-12-23 00:22:44 +0700

    update introduce-myself

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -15,26 +15,27 @@ function Home2() {
             </h1>
             <p className="home-about-body">
               I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              something, I think… 🤔
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Golang. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
+                  {" "}
+                  C++, Javascript/Typescript and Golang.{" "}
                 </b>
               </i>
               <br />
               <br />
+              My field of Interest's are building new
+              <i>
+                <b className="purple">Web Technologies, Blockchain </b> and also
+                in areas related to{" "}
+                <b className="purple">Data Structures & Algorithms</b>
+              </i>
+              <br />
+              <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              with <b className="purple">Golang</b> and
               <i>
                 <b className="purple">
                   {" "}
@@ -43,7 +44,7 @@ function Home2() {
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> React.js</b>
               </i>
             </p>
           </Col>
